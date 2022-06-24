430e151d3273403c7361dee7aad17ac57110726f
Christopher <cjeong1021@gmail.com>
2022-06-24 12:07:41 -0400

    testing

--- petstagram/src/App.js
+++ petstagram/src/App.js
@@ -2,7 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <h1>testing</h1>
+    </div>
+  );
 }
 
 export default App;
