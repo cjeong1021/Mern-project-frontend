9d2ec8fb58896861037435d011a5f28b0542c914
sonam <tseringsonam34@hotmail.com>
2022-06-24 14:30:32 -0400

    home button functions

--- petstagram/src/navigation/Navigation.js
+++ petstagram/src/navigation/Navigation.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './navigation.css'
 import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
 import { IoIosAddCircleOutline, IoIosSearch } from 'react-icons/io'
+import { Link} from 'react-router-dom'
 
 const Navigation = () => {
   return (
@@ -12,7 +13,9 @@ const Navigation = () => {
             <input type="search" className='searchBar' placeholder='Search'/>
         </form>
         <div >
+          <Link to="/" style={{color: 'inherit'}} >
           <AiOutlineHome size={20} className='icons' />
+          </Link>
           <IoIosAddCircleOutline size={20} className='icons' />
           <AiOutlineUser size={20} className='icons' />
         </div>
