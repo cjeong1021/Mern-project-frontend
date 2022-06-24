66646cd6e9e6d695a1f9f6bbd5b4daa940ad0dfe
sonam <tseringsonam34@hotmail.com>
2022-06-24 12:03:39 -0400

    test

--- petstagram/src/App.js
+++ petstagram/src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return <div className='App'>
+    <h1>testing push</h1>
+  </div>;
 }
 
 export default App;
