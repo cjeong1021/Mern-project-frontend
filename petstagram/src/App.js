b5890256408fda5d9dfc61f74ff9553700314be6
Mehmet Tansu Soydan <aidenshaw.leo@gmail.com>
2022-06-24 12:10:28 -0400

    mehmet test

--- petstagram/src/App.js
+++ petstagram/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
-      <h1>testing</h1>
+    <div className="App">
+      <h1>testing2</h1>
     </div>
   );
 }
