3ca8dd045cf437dab3bcd50893de92393579990c
Christopher <cjeong1021@gmail.com>
2022-06-27 14:00:08 -0400

    link db to posts

--- petstagram/src/App.js
+++ petstagram/src/App.js
@@ -2,95 +2,95 @@ import { useEffect, useState } from 'react';
 import React from 'react';
 import axios from 'axios';
 import './App.css';
-import {Route, Routes, Link} from 'react-router-dom'
+import { Route, Routes, Link } from 'react-router-dom';
 import Main from './Main/Main';
 import Navigation from './navigation/Navigation';
 import Login from './Login/Login';
 import SignUp from './SignUp/SignUp';
 
 function App() {
-  const [data, setData] = useState([])
-  
-    const getData = () => {
-      axios.get("https://jsonplaceholder.typicode.com/posts")
-      .then(res => {
-        setData(res.data)
+  const [data, setData] = useState([]);
+
+  const getData = () => {
+    axios
+      .get('http://localhost:8000/petstagram/posts')
+      .then((res) => {
+        setData(res.data);
       })
-      .catch(err =>{
-        console.log(err)
-      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  //UserData
+  const [userData, setUserData] = useState([]);
+
+  //User logged in
+  const [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    age: null,
+    userId: null,
+    password: '',
+    email: '',
+    logIn: false,
+  });
+
+  //Login
+  const [loginForm, setLoginForm] = useState({
+    userId: '',
+    password: '',
+  });
+
+  const handleLogin = (e) => {
+    setLoginForm({
+      ...loginForm,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const validateLogin = () => {
+    const user = userData.find((user) => user.userId === loginForm.userId);
+    if (user.password === loginForm.password) {
+      console.log('welcome');
+      setUser({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        age: user.age,
+        userId: user.userId,
+        password: user.password,
+        email: user.email,
+        logIn: true,
+      });
+    } else {
+      alert('The password you’ve entered is incorrect.');
     }
-    useEffect(() => {
-      getData()
-      }, [])
+  };
 
-      //UserData
-      const [userData, setUserData] = useState([])
+  //Sign Up
+  const [signUpForm, setSignUpForm] = useState({
+    firstName: '',
+    lastName: '',
+    age: '',
+    userId: '',
+    password: '',
+    email: '',
+    logIn: false,
+  });
 
-      //User logged in
-      const [user, setUser] = useState({
-      firstName: "",
-      lastName: "",
-      age: null,
-      userId: null,
-      password: "",
-      email: "",
-      logIn: false
-      })
+  const handleSignUp = (e) => {
+    setSignUpForm({
+      ...signUpForm,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-      //Login
-      const [loginForm, setLoginForm] = useState({
-        userId: "",
-        password: ""
-      })
-    
-      const handleLogin = (e) => {  
-        setLoginForm({
-          ...loginForm,
-          [e.target.name]: e.target.value   
-        })
-      }
-    
-      const validateLogin = () => {
-        const user = userData.find((user) => user.userId === loginForm.userId)
-        if(user.password == loginForm.password) {
-          console.log("welcome");
-          setUser({
-            firstName: user.firstName,
-            lastName: user.lastName,
-            age: user.age,
-            userId: user.userId,
-            password: user.password,
-            email: user.email,
-            logIn: true,
-          })
-        } else {
-          alert("The password you’ve entered is incorrect.");
-        }
-      }
-
-      //Sign Up
-      const [signUpForm, setSignUpForm] = useState({
-        firstName: "",
-        lastName: "",
-        age: "",
-        userId: "",
-        password: "",
-        email: "",
-        logIn: false
-      })
-
-      const handleSignUp = (e) => {  
-        setSignUpForm({
-          ...signUpForm,
-          [e.target.name]: e.target.value   
-        })
-      }
-
-      const createUser = () => {
-        axios.post('http://localhost:3000/api/users/', signUpForm)
-      }
-
+  const createUser = () => {
+    axios.post('http://localhost:8000/petstagram/users', signUpForm);
+  };
 
   return (
     <div className='App'>
@@ -99,9 +99,19 @@ function App() {
       </nav>
       <main>
         <Routes>
-          <Route path="/" element={<Main data={data}/>}/>
-          <Route path="/login" element={<Login handleLogin={handleLogin} validateLogin={validateLogin}/>} />
-          <Route path="sign-up" element={<SignUp handleSignUp={handleSignUp} createUser={createUser} />} />
+          <Route path='/' element={<Main data={data} />} />
+          <Route
+            path='/login'
+            element={
+              <Login handleLogin={handleLogin} validateLogin={validateLogin} />
+            }
+          />
+          <Route
+            path='sign-up'
+            element={
+              <SignUp handleSignUp={handleSignUp} createUser={createUser} />
+            }
+          />
         </Routes>
       </main>
     </div>
--- petstagram/src/Main/Post.js
+++ petstagram/src/Main/Post.js
@@ -1,35 +1,44 @@
-import React, {useState} from 'react'
-import { IoIosHeartEmpty, IoIosHeart } from 'react-icons/io'
-import { BsBookmark } from 'react-icons/bs'
-import { BiUserCircle } from 'react-icons/bi'
-import './post.css'
+import React, { useEffect, useState } from 'react';
+import { IoIosHeartEmpty, IoIosHeart } from 'react-icons/io';
+import { BsBookmark } from 'react-icons/bs';
+import { BiUserCircle } from 'react-icons/bi';
+import './post.css';
+import axios from 'axios';
 
-const Post = ({post}) => {
+const Post = ({ post }) => {
   const [isLiked, setIsLiked] = useState(false);
+  const [userData, setUserData] = useState({});
 
+  const getUser = () => {
+    axios
+      .get(`http://localhost:8000/petstagram/users/${post.user}`)
+      .then((res) => {
+        setUserData(res.data);
+      });
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return (
     <div className=''>
-    <div className='postBox'>
-          <div className='postTop'>
-            <BiUserCircle className='userName' size={80}/>
-            <p className='userName'>Sonam</p>
-          </div>
-        <img className='postImage' src='https://www.thesprucepets.com/thmb/jschPYYgX2Z2R04YkFt_HBIblJw=/1566x1044/filters:fill(auto,1)/GettyImages-1214648223-a185de9c88984ca69f1ac720bdea6efe.jpg' alt='#' />
+      <div className='postBox'>
+        <div className='postTop'>
+          <BiUserCircle className='userName' size={80} />
+          <p className='userName'>{userData.name}</p>
+        </div>
+        <img className='postImage' src={post.picture} alt='#' />
         <div className='postIcon'>
           <p className='likeButton' onClick={() => setIsLiked(!isLiked)}>
-           {isLiked
-              ? <IoIosHeart size={40} />
-              : <IoIosHeartEmpty size={40} />
-            }
+            {isLiked ? <IoIosHeart size={40} /> : <IoIosHeartEmpty size={40} />}
           </p>
-          
+
           <BsBookmark size={35} />
         </div>
+      </div>
     </div>
-    </div>
+  );
+};
 
-  )
-}
-
-export default Post
+export default Post;
