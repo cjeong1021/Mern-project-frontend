2b01d03a59ee1940bdacac9c19fa6cdc3461fcab
Christopher <cjeong1021@gmail.com>
2022-06-28 16:19:18 -0400

    add shop

--- petstagram/src/navigation/Navigation.js
+++ petstagram/src/navigation/Navigation.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import "./navigation.css";
 import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
@@ -22,6 +23,42 @@ const Navigation = () => {
             <h3>
               <FaPaw size={25} /> Petstagram
             </h3>
+=======
+import React from 'react'
+import './navigation.css'
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
+import { IoIosAddCircleOutline, IoIosSearch } from 'react-icons/io'
+import { FaPaw } from 'react-icons/fa'
+import { TiShoppingCart } from 'react-icons/ti'
+import { Link} from 'react-router-dom'
+
+const Navigation = () => {
+  return (
+    <div style={{backgroundColor:"white",borderBottom:'.5px solid rgb(172, 172, 172)'
+    }}>
+    <div className='navBar'>
+      <div className='logo'>
+      <Link to="/main" style={{textDecoration:'none',color: 'inherit'}} >
+        <h3><FaPaw size={25}/> Petstagram</h3>
+      </Link>
+      </div>
+        <form>
+          <IoIosSearch />
+            <input type="search" className='searchBar' placeholder='Search'/>
+        </form>
+        <div >
+          <Link to="/main" style={{color: 'inherit'}} >
+          <AiOutlineHome size={28} className='icons' />
+          </Link>
+          <Link to="/shop" style={{color: 'inherit'}} >
+          <TiShoppingCart size={28} className='icons' />
+          </Link>
+          <Link to="/post-input" style={{color: 'inherit'}} >
+          <IoIosAddCircleOutline size={28} className='icons' />
+          </Link>
+          <Link to="/user-profile" style={{color: 'inherit'}} >
+          <AiOutlineUser size={28} className='icons' />
+>>>>>>> 50daf78 (add shop)
           </Link>
         </div>
         <form>
