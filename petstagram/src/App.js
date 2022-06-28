01af6a8dfd97c63774b9061758b8c16b79de92cc
sonam <tseringsonam34@hotmail.com>
2022-06-28 14:22:49 -0400

    fixed picture width

--- petstagram/src/App.js
+++ petstagram/src/App.js
@@ -7,7 +7,7 @@ import Main from './Main/Main';
 import Navigation from './navigation/Navigation';
 import Login from './Login/Login';
 import SignUp from './SignUp/SignUp';
-import PostInput from './PostInput/Upload';
+import Upload from './PostInput/Upload';
 import UserProfile from './UserProfile/UserProfile';
 
 function App() {
