3be2fb3a2afd99fecdd9d734cb35a8de0ca0aca6
Christopher <cjeong1021@gmail.com>
2022-06-28 09:44:36 -0400

    comment at bottom of div

--- petstagram/src/Main/Post.js
+++ petstagram/src/Main/Post.js
@@ -39,6 +39,7 @@ const Post = () => {
         </div>
         <div >
 <<<<<<< HEAD
+<<<<<<< HEAD
           <p className='commentSection'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita iusto non hic accusamus temporibus quia vel, tempore rem. Quas beatae, vitae distinctio enim ullam eaque! Autem alias cupiditate reiciendis dolor!</p>
           <br />
           <input className='comment' type="text" placeholder='comment...' />
@@ -47,6 +48,11 @@ const Post = () => {
           <br />
           <p className='commentSection'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita iusto non hic accusamus temporibus quia vel, tempore rem. Quas beatae, vitae distinctio enim ullam eaque! Autem alias cupiditate reiciendis dolor!</p>
 >>>>>>> 5a79f9d (postbox adjustments)
+=======
+          <p className='commentSection'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita iusto non hic accusamus temporibus quia vel, tempore rem. Quas beatae, vitae distinctio enim ullam eaque! Autem alias cupiditate reiciendis dolor!</p>
+          <br />
+          <input className='comment' type="text" placeholder='comment...' />
+>>>>>>> 824880c (comment at bottom of div)
         </div>
         <div className='captionSection'> 
             <strong>Likes</strong> : ###
